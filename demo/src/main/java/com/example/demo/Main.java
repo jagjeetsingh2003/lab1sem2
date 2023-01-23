ba5f9dcdda5a28741644521258ae822f99e19c22
jagjeetsingh2003 <113151448+jagjeetsingh2003@users.noreply.github.com>
2023-01-22 23:50:31 -0500

    Add files via upload

---
+++ demo/src/main/java/com/example/demo/Main.java
@@ -0,0 +1,7 @@
+package com.example.demo;
+
+public class Main {
+    public static void main(String[] args) {
+        Student student1 = new Student(firstName:"jagjeet",lastName:"singh");
+    }
+}
---
+++ demo/src/main/java/com/example/demo/Student.java
@@ -0,0 +1,14 @@
+package com.example.demo;
+
+public class Student {
+    public String firstName;
+    public String lastName;
+    public int studentNumber;
+
+    public Student(String firstName, String lastName, String studentNumber) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.studentNumber = studentNumber;
+    }
+}
+
